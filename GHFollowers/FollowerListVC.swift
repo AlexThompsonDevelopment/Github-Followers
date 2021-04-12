065afabb259e0caccd287063a6c933df68857ad6
Alex <alexthompsondevelopment@gmail.com>
2021-04-12 15:59:54 +1000

    build out search page

---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,24 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Alexander Thompson on 12/4/21.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+  
+    }
+    
+
+}
