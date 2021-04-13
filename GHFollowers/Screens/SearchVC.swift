75b9d9a7d555b128942b6ac5028199430b77e50c
Alex <alexthompsondevelopment@gmail.com>
2021-04-13 12:01:43 +1000

    set up networking

--- GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/Screens/SearchVC.swift
@@ -28,7 +28,7 @@ class SearchVC: UIViewController {
   
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     func createDismissKeyboardTapGesture() {
@@ -39,7 +39,7 @@ class SearchVC: UIViewController {
     @objc func pushFollowerListVC() {
         
         guard isUserNameEntered else {
-            print("no username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for", buttonTitle: "Ok")
             return }
         let followerListVC       = FollowerListVC()
         followerListVC.username  = userNameTextField.text
